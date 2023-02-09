7b9da7ccf95bb0d112bb5e1c0883af13409ca6ee
Jakub Ogonowski <76257496+jogonowski297@users.noreply.github.com>
2023-02-10 00:07:42 +0100

    Update EndPoints.kt

--- app/src/main/java/com/example/adarp/EndPoints.kt
+++ app/src/main/java/com/example/adarp/EndPoints.kt
@@ -1,18 +1,18 @@
 package com.example.adarp
 
 object EndPoints {
-    val URL_ADD_ARTIST = "https://adarp.pl/app/postSomething/addTask.php"
-    val URL_ADD_TASKS_CLOSED = "https://adarp.pl/app/postSomething/addClosedTask.php"
+    val URL_ADD_ARTIST = "p"
+    val URL_ADD_TASKS_CLOSED = ""
 
 
-    val URL_GET_COMPANY = "https://adarp.pl/app/spinner/companySpinner.php"
-    val URL_GET_COLOR_USERS = "https://adarp.pl/app/getSomething/getColor.php"
-    val URL_GET_WORKERS = "https://adarp.pl/app/spinner/workersSpinner.php"
-    val URL_GET_TASKS = "https://adarp.pl/app/listView/tasksView.php"
-    val URL_DEL_TASK = "https://adarp.pl/app/postSomething/removeTask.php"
-    val URL_EDIT_TASK = "https://adarp.pl/app/postSomething/editTask.php"
-    //    val URL_GET_TASKS = "https://crm.adarp.eu/crm_mobile/app/listView/tasksView.php"
-    val URL_GET_COPIES = "https://adarp.pl/app/listView/copiesView.php"
+    val URL_GET_COMPANY = ""
+    val URL_GET_COLOR_USERS = "p"
+    val URL_GET_WORKERS = ""
+    val URL_GET_TASKS = ""
+    val URL_DEL_TASK = "p"
+    val URL_EDIT_TASK = ""
+    //    val URL_GET_TASKS = ""
+    val URL_GET_COPIES = ""
 
 
 }
